bc43d9e2e61034f5021b169e6dd670be1afaa258
Navneet <s1rha7@hotmail.co.uk>
2021-12-28 20:47:24 +0000

    axios data setup

---
+++ components/Card.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="card">
+    <img :src="image" :alt="title" width="150" height="150" />
+    <h3>{{ title }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: null
+    },
+    image: {
+      type: String,
+      default: null
+    }
+  }
+}
+</script>
+
+<style scoped>
+.card {
+  border: 1px solid black;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,15 +2,24 @@
   <div>
     <Header />
     <main>
+      <Card
+        v-for="mountain in mountains"
+        :key="mountain.slug"
+        :title="mountain.title"
+        :image="mountain.image"
+      />
+
       <ul>
-        <li>Get mountains api data from api.nuxt.dev</li>
-        <li>Create Card component</li>
-        <li>Loop cards</li>
+        <li>✅ Get mountains api data from api.nuxt.dev - https://api.nuxtjs.dev/mountains</li>
+        <li>✅ Create Card component</li>
+        <li>✅ Loop cards</li>
         <li>Setup Vercel</li>
         <li>Setup dynamic pages</li>
+        <li>Move data to the store. Pass in data via props</li>
         <li>Setup Tailwind CSS</li>
         <li>CSS custom properties and a dark mode toggle</li>
         <li>Gsap animations</li>
+        <li>Google fonts</li>
       </ul>
     </main>
     <Footer />
@@ -19,6 +28,24 @@
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+
+  data() {
+    return {
+      mountains: null
+    }
+  },
+
+  async asyncData({ $axios }) {
+    const mountains = await $axios.$get(`https://api.nuxtjs.dev/mountains`);
+    return { mountains };
+    this.mountains = data;
+  }
+
+  // mounted() {
+  //   const data = this.$axios.$get('https://api.nuxtjs.dev/mountains');
+  //   console.log(data);
+  //   this.mountains = data;
+  // }
 }
 </script>

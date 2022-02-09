dd4f4c10dd3ed52b82313c9c665afca586595a16
Navneet <s1rha7@hotmail.co.uk>
2022-02-09 15:33:20 +0000

    dist edits

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,10 +1,14 @@
 export default {
-  // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
-  // Global page headers: https://go.nuxtjs.dev/config-head
+  ssr: false,
+
+  generate: {
+    fallback: true
+  },
+
   head: {
-    title: 'my-mountains-app',
+    title: 'Mountains',
     htmlAttrs: {
       lang: 'en'
     },
@@ -19,30 +23,24 @@ export default {
     ]
   },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
     '@/assets/css/main.css'
   ],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
   ],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/tailwindcss
     '@nuxtjs/tailwindcss',
   ],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     '@nuxtjs/axios'
   ],
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
   }
 }
